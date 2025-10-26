a08d3424d546de3db5e8b1bfb25099c83b53d765
Sergiu Tigan <sergiu@tigan.dev>
2025-10-26 05:34:23 +0200

    Testing admin access

--- src/server.js
+++ src/server.js
@@ -76,7 +76,7 @@ app.use(session({
     maxAge: 24 * 60 * 60 * 1000, // 24 hours
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: process.env.NODE_ENV === 'production' ? 'strict' : 'lax'
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax'
   }
 }));
 
